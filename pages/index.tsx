284b05be29dbe2be8fff261a66e3db01680a9256
F-Sotomayor <facundosotomayor95@hotmail.com>
2020-11-15 17:02:31 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
-import {Heading, Stack, Text, Image, Box, Flex, Button, Container, Input} from "@chakra-ui/core";
+import {
+  Heading,
+  Stack,
+  Text,
+  Image,
+  Box,
+  Flex,
+  Container,
+  Input,
+  StackDivider,
+} from "@chakra-ui/core";
 import {GetServerSideProps} from "next";
 
 import api from "../product/api";
@@ -10,97 +20,113 @@ interface Props {
 }
 
 const IndexPage: React.FC<Props> = ({products}) => (
-  <Container
-    display="flex"
-    height="auto"
-    justifyContent="center"
-    maxWidth="100vw"
-    overflowX="hidden"
-    padding={0}
-  >
-    <Stack flex={1} height="100%" maxWidth="90vw">
+  <Container height="auto" maxWidth="100vw" minHeight="100vh" overflowX="hidden" padding={0}>
+    <Stack flex={1} height="100%" margin="auto" maxWidth={800} paddingX={4}>
       <Heading height="15vh">
         <Image height="100%" src="logo.jpg" width="auto" />
       </Heading>
-      <Box display="flex" flexDirection="column" height="100%">
+      <Stack height="100%" spacing={12}>
         {products.map((product) => (
-          <Flex
+          <Stack
             key={product.id}
-            backgroundColor="rgba(0,0,0,0.2)"
-            borderRadius={12}
-            direction="column"
+            backgroundColor="gray.50"
+            borderColor="gray.100"
+            borderRadius="lg"
+            borderWidth={1}
+            boxShadow="2xl"
+            divider={<StackDivider />}
             flex={1}
-            marginBottom={4}
             padding={4}
+            spacing={4}
           >
-            <Flex>
+            <Stack direction="row" spacing={4}>
               <Box height="auto" maxHeight="100%">
-                <Image borderRadius={12} height={120} src="pantest.png" />
+                <Image borderRadius="lg" height={24} src="pantest.png" />
               </Box>
-              <Flex direction="column" justifyContent="space-between" marginLeft={12}>
-                <Text fontSize={[16, 16, 16, 24]}>{product.title}</Text>
-                <Text fontSize={[12, 12, 12, 20]}>{product.description}</Text>
+              <Stack spacing={2}>
+                <Stack spacing={0}>
+                  <Text fontSize="2xl" fontWeight={500} lineHeight="normal">
+                    {product.title}
+                  </Text>
+                  <Text fontSize="lg">{product.description}</Text>
+                </Stack>
                 <Text
-                  backgroundColor="rgba(0,0,0,0.9)"
-                  borderRadius={12}
+                  backgroundColor="gray.800"
+                  borderRadius={9999}
                   color="white"
-                  fontSize={[12, 12, 12, 16]}
+                  fontSize="sm"
                   height="auto"
                   textAlign="center"
                   width={100}
                 >
                   Stock : {product.stock}
                 </Text>
-              </Flex>
-            </Flex>
-            <Flex direction="column" padding={2}>
+              </Stack>
+            </Stack>
+            <Stack spacing={0}>
               <Flex justifyContent="space-between" width="100%">
                 <Box flex={0.25}>
-                  <Text fontSize={[12, 12, 12, 16]} textAlign="left">
+                  <Text fontSize="xs" fontWeight={500} textAlign="left" textTransform="uppercase">
                     Unidades
                   </Text>
                 </Box>
                 <Box flex={0.25}>
-                  <Text fontSize={[12, 12, 12, 16]} textAlign="left">
+                  <Text fontSize="xs" fontWeight={500} textAlign="left" textTransform="uppercase">
                     Precio unitario
                   </Text>
                 </Box>
                 <Box flex={0.25}>
-                  <Text fontSize={[12, 12, 12, 16]} textAlign="left">
+                  <Text fontSize="xs" fontWeight={500} textAlign="left" textTransform="uppercase">
                     Cantidad
                   </Text>
                 </Box>
                 <Box flex={0.25}>
-                  <Text fontSize={[12, 12, 12, 16]} textAlign="right">
+                  <Text fontSize="xs" fontWeight={500} textAlign="right" textTransform="uppercase">
                     Precio Total
                   </Text>
                 </Box>
               </Flex>
-              {product.presentations.map((presentation) => (
-                <Flex
-                  key={presentation.units}
-                  alignItems="center"
-                  justifyContent="space-between"
-                  width="100%"
-                >
-                  <Text flex={0.25} fontSize={[12, 12, 12, 16]}>
-                    {presentation.units}
-                  </Text>
-                  <Text flex={0.25} fontSize={[12, 12, 12, 16]}>
-                    {presentation.price}
-                  </Text>
-                  <Box flex={0.25}>
-                    <Input borderColor="primary.300" marginY={1} type="number" />
-                  </Box>
-                  <Text flex={0.25} fontSize={[12, 12, 12, 16]} textAlign="right">
-                    $ 15151
-                  </Text>
-                </Flex>
-              ))}
-            </Flex>
-          </Flex>
+              <Stack divider={<StackDivider />} spacing={1}>
+                {product.presentations.map((presentation) => (
+                  <Flex
+                    key={presentation.units}
+                    alignItems="center"
+                    justifyContent="space-between"
+                    width="100%"
+                  >
+                    <Text flex={0.25} fontSize={[12, 12, 12, 16]}>
+                      {presentation.units}
+                    </Text>
+                    <Text flex={0.25} fontSize={[12, 12, 12, 16]}>
+                      {presentation.price.toLocaleString("es-AR", {
+                        style: "currency",
+                        currency: "ARS",
+                      })}
+                    </Text>
+                    <Box flex={0.25}>
+                      <Input
+                        borderColor="primary.300"
+                        colorScheme="primary"
+                        marginY={1}
+                        maxWidth={100}
+                        size="sm"
+                        type="number"
+                        variant="filled"
+                      />
+                    </Box>
+                    <Text flex={0.25} fontSize={[12, 12, 12, 16]} textAlign="right">
+                      {Number(1500).toLocaleString("es-AR", {
+                        style: "currency",
+                        currency: "ARS",
+                      })}
+                    </Text>
+                  </Flex>
+                ))}
+              </Stack>
+            </Stack>
+          </Stack>
         ))}
-      </Box>
+      </Stack>
     </Stack>
   </Container>
 );
